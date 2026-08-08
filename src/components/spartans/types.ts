ba00cf6f49bf29f80329eebccdd54d94929d6554
dodotnecio <dodotnecio@gmail.com>
2026-08-09 07:37:22 +0800

    Create types.ts

---
+++ src/components/spartans/types.ts
@@ -0,0 +1,13 @@
+export type View =
+  | "home"
+  | "about"
+  | "login"
+  | "register"
+  | "register-admin"
+  | "member-profile"
+  | "member-code"
+  | "member-records"
+  | "admin-profile"
+  | "admin-codegen"
+  | "admin-update"
+  | "admin-report";
